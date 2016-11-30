d0c77f2379ebbc4de219f823f18a012752e508aa
Greg Williams <grw300@gmail.com>
2016-11-30 14:06:57 -0600

    Move to a button instead of MenuItem to allow for disable - this binding will have to be tested

---
+++ rivER_app/rivER/Views/Converters/StateToEnabledConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace rivER
+{
+    public class StateToEnabledConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            /*
+             * Only enabled when the State has no value
+             */
+            var valueAsBool = (bool?)value;
+            return !valueAsBool.HasValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
